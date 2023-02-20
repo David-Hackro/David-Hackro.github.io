19598aa1089fbaaeba21749e952ba6f66585ca14
David Hackro <hackro91@gmail.com>
2023-02-20 16:48:08 -0600

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'David-Hackro', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 6, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
@@ -23,16 +23,7 @@ const config = {
     fileUrl:
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
-  
-  /*pragmatic : [
-  'Code guidelines',
- 'Documentation',
- 'Algorithms',
- 'Bugs resolution',
- 'Code review',
- 'Build release',
-  'Clean Architecture',
-  ],*/
+ 
    skills: [
     'Kotlin',
     'Scriptin',
@@ -52,7 +43,13 @@ const config = {
     'Repository',
     'Observer',
     'Singleton',
-    'Clean Architecture',
+'Code guidelines',
+ 'Documentation',
+ 'Algorithms',
+ 'Bugs resolution',
+ 'Code review',
+ 'Build release',
+  'Clean Architecture',
   ],
   experiences: [
     {
@@ -126,11 +123,11 @@ const config = {
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
-  blog: {
+  /*blog: {
     source: 'dev', // medium | dev
     username: 'davidhackro', // to hide blog section, keep it empty
     limit: 2, // How many posts to display. Max is 10.
-  },
+  },*/
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
@@ -140,7 +137,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'emerald',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
