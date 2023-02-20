800d4af60724222462b653d67e51bfd5e81ee345
David Hackro <hackro91@gmail.com>
2023-02-20 16:07:19 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindConfig from './tailwind.config.mjs';
 // https://vitejs.dev/config/
 export default defineConfig({
   // If you are deploying to https://David-Hackro.github.io/, set base to '/'.
-  // If you are deploying to https://David-Hackro.github.io/David-Hackro/, for example your repository is at https://github.com/David-Hackro/David-Hackro, then set base to '/<REPO>/'.
+  // If you are deploying to https://David-Hackro.github.io/David-Hackro.github.io/, for example your repository is at https://github.com/David-Hackro/David-Hackro.github.io, then set base to '/David-Hackro.github.io/'.
   base: '/gitprofile/',
   plugins: [react()],
   css: {
