e660f9ad9c3c3b30cce2d4f2955d546429f47675
David Hackro <hackro91@gmail.com>
2023-02-28 14:55:54 -0600

    Update index.jsx

--- src/components/otherskills/index.jsx
+++ src/components/otherskills/index.jsx
@@ -1,7 +1,7 @@
 import { skeleton } from '../../helpers/utils';
 import PropTypes from 'prop-types';
 
-const othersSkill = ({ loading, othersSkills }) => {
+const Software = ({ loading, softwares }) => {
   const renderSkeleton = () => {
     let array = [];
     for (let index = 0; index < 12; index++) {
@@ -17,7 +17,7 @@ const othersSkill = ({ loading, othersSkills }) => {
 
   return (
     <>
-      {othersSkills?.length !== 0 && (
+      {softwares?.length !== 0 && (
         <div className="card shadow-lg compact bg-base-100">
           <div className="card-body">
             <div className="mx-3">
@@ -26,7 +26,7 @@ const othersSkill = ({ loading, othersSkills }) => {
                   skeleton({ width: 'w-32', height: 'h-8' })
                 ) : (
                   <span className="text-base-content opacity-70">
-                    Tech Stack
+                    Android Stack
                   </span>
                 )}
               </h5>
@@ -35,12 +35,12 @@ const othersSkill = ({ loading, othersSkills }) => {
               <div className="-m-1 flex flex-wrap justify-center">
                 {loading
                   ? renderSkeleton()
-                  : othersSkills.map((otherskill, index) => (
+                  : softwares.map((skill, index) => (
                       <div
                         key={index}
                         className="m-1 text-xs inline-flex items-center font-bold leading-sm px-3 py-1 badge-primary bg-opacity-90 rounded-full"
                       >
-                        {otherskill}
+                        {skill}
                       </div>
                     ))}
               </div>
@@ -52,9 +52,9 @@ const othersSkill = ({ loading, othersSkills }) => {
   );
 };
 
-othersSkill.propTypes = {
+Software.propTypes = {
   loading: PropTypes.bool.isRequired,
-  othersSkills: PropTypes.array.isRequired,
+  softwares: PropTypes.array.isRequired,
 };
 
-export default othersSkill;
+export default Software;
