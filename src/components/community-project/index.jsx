de1b7457554dec61cca2975018a707e20d67f481
David-Hackro <hackro91@gmail.com>
2023-07-17 18:30:40 -0600

    add new section

--- src/components/community-project/index.jsx
+++ src/components/community-project/index.jsx
@@ -142,7 +142,7 @@ const CommunityProject = ({ communityProjects, loading, googleAnalytics }) => {
                         skeleton({ width: 'w-40', height: 'h-8' })
                       ) : (
                         <span className="text-base-content opacity-70">
-                          My Projects
+                          Community Projects
                         </span>
                       )}
                     </h5>
